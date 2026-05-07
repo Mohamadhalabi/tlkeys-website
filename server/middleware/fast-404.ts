5ee394e498e74a119eaebcbcfd13c1ee76ecf3c7
Mohamadhalabi <mhmd_halabi99@hotmail.com>
2026-05-07 14:29:10 +0300

    fast-404.ts updated

--- server/middleware/fast-404.ts
+++ server/middleware/fast-404.ts
@@ -1,22 +1,22 @@
 // server/middleware/fast-404.ts
 export default defineEventHandler((event) => {
-    // SWR Crash Protection
     if (event.node.res.headersSent) return;
 
     const url = event.node.req.url;
-
-    // If there's no URL, just continue
     if (!url) return;
 
-    // Block requests for common static extensions that don't exist
     if (url.match(/\.(php|jpg|jpeg|png|gif|pdf|env|txt|sql|zip|json|mp4|webp|ico)$/i)) {
 
-        // ---> THE FIX: Added '/_ipx/' to the safe list so Nuxt Image works <---
-        if (url.includes('sitemap') || url.includes('/_nuxt/') || url.includes('/_ipx/')) {
+        // ADD url.includes('/_i18n/') HERE
+        if (
+            url.includes('sitemap') ||
+            url.includes('/_nuxt/') ||
+            url.includes('/_ipx/') ||
+            url.includes('/_i18n/')
+        ) {
             return;
         }
 
-        // Instantly return a raw 410 without booting up Vue SSR
         setResponseStatus(event, 410);
         return 'Gone';
     }
